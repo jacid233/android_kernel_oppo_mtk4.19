eb7b2c88be2b676e4b0e8f6517511918a0e5dd29
jacid233 <jacid11@outlook.com>
2023-05-06 19:15:36 +0800

    script: update build kernel script

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,7 +3,7 @@
 mkdir out
 
 BUILD_CROSS_COMPILE=/media/huaji/fc1e4abe-0fb6-45f0-a812-4410e3006a32/Lineage18.1/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-CLANG_PATH=/media/huaji/fc1e4abe-0fb6-45f0-a812-4410e3006a32/Lineage18.1/prebuilts/clang/host/linux-x86/clang-r383902b1/bin
+CLANG_PATH=/media/huaji/fc1e4abe-0fb6-45f0-a812-4410e3006a32/Lineage19.1/prebuilts/clang/host/linux-x86/clang-r383902/bin
 CROSS_COMPILE_ARM32=/media/huaji/fc1e4abe-0fb6-45f0-a812-4410e3006a32/Lineage18.1/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 
 CLANG_TRIPLE=aarch64-linux-gnu-
